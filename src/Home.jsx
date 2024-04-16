26804f5426e34b354aed00b9f507b2880e6057f9
Bipeshkhadka <bipeshkhadka2003@gmail.com>
2024-04-16 23:46:21 +0545

    added prop and components

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+// import Button from './Button';
 
 const num = 69;
 const Home = () => {
   return (
-    <div>
-      <h1 className='my_name'>This is the home page!</h1>
-      <h2 className='my_name'>welcome ! Bipesh {num}</h2>
-    </div>
+    <>
+        <div>
+            <h1 className='my_name'>This is the home page!</h1>
+            <h2 className='my_name'>welcome ! Bipesh {num}</h2>
+        </div>
+        {/* <Button title='animal' description='This button is to be crazy like animal'/> */}
+    </>
   )
 }
 
